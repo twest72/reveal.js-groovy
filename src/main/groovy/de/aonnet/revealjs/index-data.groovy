2c9a3f2decf68a41868209b932f6c8128ccc665e
Thomas Westphal <thomas.westphal@aonnet.de>
2012-06-28 07:05:14 +0200

    Extract js code for pretty code example in a separate file.

--- src/main/groovy/de/aonnet/revealjs/index-data.groovy
+++ src/main/groovy/de/aonnet/revealjs/index-data.groovy
@@ -135,29 +135,7 @@ data.sections.section {
     h2 'Pretty Code'
     pre {
         code(contenteditable: '') {
-            mkp.yieldUnescaped """
-var supports3DTransforms =  document.body.style['webkitPerspective'] !== undefined ||
-        document.body.style['MozPerspective'] !== undefined ||
-        document.body.style['perspective'] !== undefined;
-
-function linkify( selector ) {
-   if( supports3DTransforms ) {
-
-       var nodes = document.querySelectorAll( selector );
-
-       for( var i = 0, len = nodes.length; i < len; i++ ) {
-           var node = nodes[i];
-
-           if( !node.className || !node.className.match( /roll/g ) ) {
-               node.className += ' roll';
-               node.innerHTML = '<span data-title="'+ node.text +'">' + node.innerHTML + '</span>';
-           }
-       };
-   }
-}
-
-linkify( 'a' );
-"""
+            mkp.yieldUnescaped new File("$scriptPath/example.js").text
         }
     }
     p {
